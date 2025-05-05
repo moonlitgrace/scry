741f080b379ae3547a0b91b32c8ec03b945d5161
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-05 07:55:47 +0530

    fix: source packages/ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-import Button from "@repo/ui/button";
+import { Button } from "@repo/ui/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <span className="text-5xl">scry</span>
-      <Button />
+      <Button>Button</Button>
     </div>
   );
 }
--- packages/ui/src/button.tsx
+++
@@ -1,6 +0,0 @@
-export default function Button() {
-  return (
-    <button className="text-5xl">Button</button>
-  )
-}
-
