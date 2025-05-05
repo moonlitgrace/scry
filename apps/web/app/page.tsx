d71f71cb62bcea9b3236a3a9e1ee17b0c0605047
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-05 10:49:43 +0530

    chore: update next config and packages/ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@repo/ui/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <span className="text-5xl">scry</span>
-      <Button>Button</Button>
     </div>
   );
 }
