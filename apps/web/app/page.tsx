0d2ea825419c60ca12b0af09ca78d01d4224a090
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-06 15:48:10 +0530

    feat: add svgr/webpack with turbo config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import LogoText from '../assets/svg/logo-text.svg'
+
 export default function Home() {
   return (
-    <div>
-      Hello
-    </div>
+    <header>
+      <Link href='/'>
+        <LogoText />
+      </Link>
+    </header>
   );
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  webpack(config) {
+    // get existing rule that handles svg imports
+    const svgLoaderRule = config.module.rules.find((rule) => {
+      return rule.test.test('.svg')
+    })
+
+    config.module.rules.push(
+      // reply existing rule only for svg imports ending with ?url
+      {
+        ...svgLoaderRule,
+        test: /\.svg$/i,
+        resourseQuery: /url/
+      },
+      // conver all other .svg imports into components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourseQuery: { not: /url/ }, // exclude if .svg?url
+        use: ['@svgr/webpack']
+      }
+    );
+
+    // ignore .svg rule from default loader
+    svgLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js'
+      }
+    }
+  }
 };
 
 export default nextConfig;
