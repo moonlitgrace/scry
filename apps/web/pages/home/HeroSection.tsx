31482b6078a81555f3356b2e4dabd3d47b482808
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-07 10:21:26 +0530

    chore: use css variables instead custom opacity color

--- apps/web/pages/home/HeroSection.tsx
+++ apps/web/pages/home/HeroSection.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     <div className="absolute inset-x-0 h-[calc(var(--spacing)*150)] opacity-50 mt-44 -z-10 w-full mask-y-from-50% mask-y-to-90%" style={{ backgroundImage: 'url(/svg/grids.svg)' }}></div>
     <HomeSectionLayout>
       <h1 className="text-6xl font-bold text-center leading-tight">Track <span className="text-muted">Hidden</span> <span className="text-destructive underline">Errors</span> in your JS Apps.</h1>
-      <h2 className="text-foreground/50">{withAngleBrackets('scry reveals what your console hides')}</h2>
+      <h2 className="text-muted-foreground">{withAngleBrackets('scry reveals what your console hides')}</h2>
       <div className="inline-flex gap-2">
         <Link href='https://github.com/moonlitgrace/scry' target="_blank">
           <Button variant='ghost'>
--- apps/web/pages/home/HowItWorksSection.tsx
+++ apps/web/pages/home/HowItWorksSection.tsx
@@ -9,14 +9,14 @@ export default function HowItWorksSection() {
   return (
     <HomeSectionLayout>
       <h3 className="font-bold text-4xl">How it works</h3>
-      <h2 className="text-foreground/50">{withAngleBrackets('in 3 simple steps')}</h2>
+      <h2 className="text-muted-foreground">{withAngleBrackets('in 3 simple steps')}</h2>
       <div className="grid grid-cols-2 gap-2 w-full">
         {steps.map((step, idx) => (
           <div key={idx} className={cn(idx === 1 && 'row-span-2', "flex flex-col gap-2")}>
             <h5 className="font-bold">{idx + 1}. {step.label}</h5>
             <div className="text-sm">
               {step.code && <CodeBlock lang={step.lang ?? 'shell'}>{step.code}</CodeBlock>}
-              {step.text && <span className="text-foreground/50">{step.text}</span>}
+              {step.text && <span className="text-muted-foreground">{step.text}</span>}
             </div>
           </div>
         ))}
