18c787580f2035dd7849e898fe7abb2f783158c6
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-07 10:20:07 +0530

    feat: pricing section component with card

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/pages/home/HeroSection";
 import HowItWorksSection from "@/pages/home/HowItWorksSection";
+import PricingSection from "@/pages/home/PricingSection";
 
 export default function Home() {
-  return <main className="flex flex-col gap-10 max-w-3xl mx-auto my-10">
-    <HeroSection />
-    <HowItWorksSection />
-  </main>
+  return (
+    <main className="flex flex-col gap-20 max-w-3xl mx-auto my-10">
+      <HeroSection />
+      <HowItWorksSection />
+      <PricingSection />
+    </main>
+  )
 }
