2fdee871bec339fb2ddc357ce6b7e53ac919120f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-08 07:51:49 +0530

    feat: add prettier config

---
+++ tools/prettier/index.js
@@ -0,0 +1,10 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+const config = {
+  plugins: [],
+  printWidth: 90,
+  singleQuote: true,
+};
+
+export default config;
