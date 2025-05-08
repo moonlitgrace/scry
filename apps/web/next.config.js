843f1dfe8dc9c68bf6ce8c756ddb28e40daac803
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-08 10:12:43 +0530

    chore: remove webpack config and update tsconfig

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,32 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-  webpack(config) {
-    // get existing rule that handles svg imports
-    const svgLoaderRule = config.module.rules.find((rule) => {
-      return rule.test?.test?.('.svg');
-    });
-
-    config.module.rules.push(
-      // reply existing rule only for svg imports ending with ?url
-      {
-        ...svgLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      // conver all other .svg imports into components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if .svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    // ignore .svg rule from default loader
-    svgLoaderRule.exclude = /\.svg$/i;
-    return config;
-  },
   turbopack: {
     rules: {
       '*.svg': {
