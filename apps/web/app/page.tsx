032f1e5e468c2b8131dd14b41e124690b944e404
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-08 07:09:18 +0530

    refactor: map through sections

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,47 @@
+import HomeSectionLayout from "@/layouts/HomeSectionLayout";
 import CTASection from "@/pages/home/CTASection";
 import FAQSection from "@/pages/home/FAQSection";
 import HeroSection from "@/pages/home/HeroSection";
 import HowItWorksSection from "@/pages/home/HowItWorksSection";
 import PricingSection from "@/pages/home/PricingSection";
+import { withAngleBrackets } from "@/utils/with-angle-brackets";
 
 export default function Home() {
   return (
     <main className="p-5 flex flex-col gap-10 md:gap-20 md:max-w-3xl md:mx-auto my-5 md:my-10">
       <HeroSection />
-      <HowItWorksSection />
-      <PricingSection />
-      <CTASection />
-      <FAQSection />
+      {sections.map(({ id, title, subText, Component }, idx) => (
+        <HomeSectionLayout key={idx} id={id}>
+          <h3 className="font-bold text-2xl md:text-4xl">{title}</h3>
+          <h2 className="text-muted-foreground text-sm md:text-base">{withAngleBrackets(subText)}</h2>
+          <Component />
+        </HomeSectionLayout>
+      ))}
     </main>
   )
 }
+
+const sections = [
+  {
+    id: 'how-it-works',
+    title: 'How It Works',
+    subText: 'in 3 simple steps',
+    Component: HowItWorksSection
+  },
+  {
+    id: 'pricing',
+    title: 'Pricing',
+    subText: 'start free. upgrade when you need more',
+    Component: PricingSection
+  },
+  {
+    title: 'Ready To DEBUG Smarter?',
+    subText: 'join developers tracking errors with scry today',
+    Component: CTASection
+  },
+  {
+    title: 'FAQ',
+    subText: 'frequently asked questions',
+    Component: FAQSection
+  },
+]
--- apps/web/pages/home/PricingSection.tsx
+++ apps/web/pages/home/PricingSection.tsx
@@ -1,5 +1,3 @@
-import HomeSectionLayout from "@/layouts/HomeSectionLayout";
-import { withAngleBrackets } from "@/utils/with-angle-brackets";
 import { Button } from "@repo/ui/components/ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@repo/ui/components/ui/card';
 import { cn } from "@repo/ui/lib/utils";
@@ -7,28 +5,24 @@ import { Check } from 'lucide-react';
 
 export default function PricingSection() {
   return (
-    <HomeSectionLayout id="pricing">
-      <h3 className="font-bold text-2xl md:text-4xl">Pricing</h3>
-      <h2 className="text-muted-foreground text-sm md:text-base text-center">{withAngleBrackets('start free. upgrade when you need more')}</h2>
-      <div className="grid md:grid-cols-2 gap-5 w-full">
-        {Object.entries(plans).map(([key, plan]) => (
-          <Card key={key} className={cn(!plan.isActive && 'opacity-50 pointer-events-none select-none', "rounded-none")}>
-            <CardHeader>
-              <CardTitle>{plan.label}</CardTitle>
-              <CardDescription>({plan.isActive ? 'active' : 'coming soon'})</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2 text-sm">
-              {plan.features.map((feat, idx) => (
-                <span key={idx} className="inline-flex items-center gap-2"><Check className="size-4 text-muted-foreground" /> {feat}</span>
-              ))}
-            </CardContent>
-            <CardFooter>
-              <Button>Get started</Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </HomeSectionLayout>
+    <div className="grid md:grid-cols-2 gap-5 w-full">
+      {Object.entries(plans).map(([key, plan]) => (
+        <Card key={key} className={cn(!plan.isActive && 'opacity-50 pointer-events-none select-none', "rounded-none")}>
+          <CardHeader>
+            <CardTitle>{plan.label}</CardTitle>
+            <CardDescription>({plan.isActive ? 'active' : 'coming soon'})</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2 text-sm">
+            {plan.features.map((feat, idx) => (
+              <span key={idx} className="inline-flex items-center gap-2"><Check className="size-4 text-muted-foreground" /> {feat}</span>
+            ))}
+          </CardContent>
+          <CardFooter>
+            <Button>Get started</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   )
 }
 
