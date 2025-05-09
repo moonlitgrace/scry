6dbc3cd1940dca23551c34831316d9aa85c16e16
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-09 20:31:42 +0530

    fix: add link to sign-up page

--- apps/web/app/(app)/_components/CTASection.tsx
+++ apps/web/app/(app)/_components/CTASection.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@repo/ui/components/ui/button';
+import Link from 'next/link';
 
 export default function CTASection() {
   return (
     <div className="mt-5 flex flex-col items-center gap-2 md:mt-0">
-      <Button size="lg">Sign up Free</Button>
+      <Button size="lg" asChild>
+        <Link href='/sign-up'>Sign up Free</Link>
+      </Button>
       <span className="text-muted-foreground text-xs">No Credit Card required</span>
     </div>
   );
