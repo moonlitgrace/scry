80557a844f4134f9d8297bada71d95d8e4d653da
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-09 14:26:56 +0530

    feat: add google icon

--- apps/web/app/(auth)/sign-in/page.tsx
+++ apps/web/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import GoogleIcon from "@/components/icons/Google";
 import { Button } from "@repo/ui/components/ui/button";
 import { Input } from "@repo/ui/components/ui/input";
 import { Label } from "@repo/ui/components/ui/label";
@@ -9,11 +10,11 @@ export default function SignIn() {
       <h4 className="font-sans text-4xl">Welcome back!</h4>
       <p className="text-sm text-muted-foreground">Scry helps you track and reveal hidden errors and logs in your JavaScript apps.</p>
       <div className="flex flex-col gap-2 mt-5">
-        <Label htmlFor="email">Email</Label>
+        <Label htmlFor="email">Email*</Label>
         <Input type="email" id="email" name="email" placeholder="Enter your email" />
       </div>
       <div className="flex flex-col gap-2">
-        <Label htmlFor="password">Password</Label>
+        <Label htmlFor="password">Password*</Label>
         <Input type="password" id="password" name="password" placeholder="Enter your password" />
       </div>
       <Button type="button">Sign In</Button>
@@ -22,7 +23,10 @@ export default function SignIn() {
         <span className="text-xs text-muted-foreground">OR</span>
         <span className="w-full border-b my-5"></span>
       </div>
-      <Button type="button" variant='outline'>Sign in with Google</Button>
+      <Button type="button" variant='outline'>
+        <GoogleIcon className="fill-muted-foreground" />
+        Continue with Google
+      </Button>
       <Link href='/sign-up' className="text-xs text-muted-foreground hover:underline">Don't have an account? Sign up</Link>
     </form>
   )
