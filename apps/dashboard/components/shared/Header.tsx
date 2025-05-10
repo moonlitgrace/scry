1ce83226e6aeca66a1e28201634e7de7bb5d2873
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-10 12:53:01 +0530

    feat: dashboard

--- apps/dashboard/components/shared/Header.tsx
+++ apps/dashboard/components/shared/Header.tsx
@@ -2,8 +2,17 @@ import Logo from '@/assets/svg/logo.svg';
 import { Avatar, AvatarFallback } from '@repo/ui/components/ui/avatar';
 import { Badge } from '@repo/ui/components/ui/badge';
 import { Button } from '@repo/ui/components/ui/button';
-import { Bell, ChevronDown } from 'lucide-react'
+import { ChevronDown, CirclePlus, LogOut } from 'lucide-react'
 import Link from 'next/link';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@repo/ui/components/ui/dropdown-menu"
+import { ModeToggle } from '@repo/ui/components/ui/mode-toggle';
 
 export default function Header({ children }: { children?: React.ReactNode }) {
   return (
@@ -31,14 +40,38 @@ export default function Header({ children }: { children?: React.ReactNode }) {
           <Button size='sm' variant='ghost' disabled>
             Docs
           </Button>
-          <Button size='icon-sm' variant='outline'>
-            <Bell />
-          </Button>
-          <Button size='icon-sm' variant='outline'>
-            <Avatar className='rounded-none'>
-              <AvatarFallback className='bg-transparent'>M</AvatarFallback>
-            </Avatar>
-          </Button>
+          <ModeToggle />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size='icon-sm' variant='outline'>
+                <Avatar className='rounded-none'>
+                  <AvatarFallback className='bg-transparent'>M</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuLabel className='text-sm flex flex-col'>
+                <span>moonlitgrace</span>
+                <span className='text-xs text-muted-foreground'>moonlitgrace.gaia@gmail.com</span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href={'/'} className='flex-1'>Dashboard</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={'/account'} className='flex-1'>Account</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className='cursor-pointer items-center justify-between'>
+                <span>Create Org</span>
+                <CirclePlus />
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className='cursor-pointer items-center justify-between'>
+                <span>Log out</span>
+                <LogOut />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </nav>
       </div>
       {children && (
