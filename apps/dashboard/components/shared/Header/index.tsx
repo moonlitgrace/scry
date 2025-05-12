8ff1304c01cc9ec6a3856173a1935ffcda6a1777
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-12 09:43:15 +0530

    feat: make org-dropdown a client component and use-params

--- apps/dashboard/components/shared/Header/index.tsx
+++ apps/dashboard/components/shared/Header/index.tsx
@@ -1,35 +1,12 @@
-import Link from 'next/link';
 import NavActions from './NavActions';
-import { Badge } from '@repo/ui/components/ui/badge';
-import { Button } from '@repo/ui/components/ui/button';
-import { ChevronDown } from 'lucide-react';
+import OrgDropdown from './OrgDropdown';
 
-interface Props {
-  id: string;
-}
-
-export default function Header({ id }: Props) {
+export default function Header() {
   return (
     <header className="bg-background/95 z-1 inline-flex w-full items-center justify-between px-5 pt-3 pl-14">
       <div className="inline-flex items-center gap-4">
         <span className="text-muted-foreground/50">/</span>
-        <div className="inline-flex items-center gap-2">
-          <Link
-            href={`/org/${id}`}
-            className="inline-flex items-center gap-2 text-sm font-medium"
-          >
-            moonlitgrace&apos;s Org
-            <Badge variant="secondary">Free</Badge>
-          </Link>
-          <Button
-            size="icon-sm"
-            variant="ghost"
-            className="size-[calc(var(--spacing)*5.5)]"
-            disabled
-          >
-            <ChevronDown />
-          </Button>
-        </div>
+        <OrgDropdown />
       </div>
       <NavActions />
     </header>
