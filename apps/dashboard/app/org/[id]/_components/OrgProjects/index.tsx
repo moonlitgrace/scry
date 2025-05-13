0fa1498d70847b465cc307c152660104a59578ee
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-13 11:39:58 +0530

    refactor: remove redudant org-projects components

--- apps/dashboard/app/org/[id]/_components/OrgProjects/index.tsx
+++ apps/dashboard/app/org/[id]/_components/OrgProjects/index.tsx
@@ -1,21 +1,20 @@
-import { getProjects } from '@/dal/projects';
-import ProjectCard from './ProjectCard';
+import { Suspense } from 'react';
+import ProjectCardList from './ProjectCardList';
+import ProjectCardListSkeleton from './ProjectCardListSkeleton';
 
-interface Props {
+export interface Props {
   query: string;
   sort: 'recent' | 'name';
 }
 
 export default async function OrgProjects({ query, sort }: Props) {
-  const projects = await getProjects(query, sort);
-
   return (
     <div className="col-span-2 flex flex-col gap-2">
       <h5 className="text-sm font-medium">Projects</h5>
       <div className="grid grid-cols-2 gap-5">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
+        <Suspense key={query + sort} fallback={<ProjectCardListSkeleton />}>
+          <ProjectCardList query={query} sort={sort} />
+        </Suspense>
       </div>
     </div>
   );
--- apps/dashboard/app/org/[id]/page.tsx
+++ apps/dashboard/app/org/[id]/page.tsx
@@ -1,8 +1,6 @@
-import { Suspense } from 'react';
 import OrgControls from './_components/OrgControls';
 import OrgProjects from './_components/OrgProjects';
 import OrgRecentErrors from './_components/OrgRecentErrors';
-import OrgProjectsSkeleton from './_components/OrgProjects/OrgProjectsSkeleton';
 
 interface Props {
   searchParams: Promise<{
@@ -21,9 +19,7 @@ export default async function Page(props: Props) {
       <OrgControls />
       <section className="grid grid-cols-3 gap-5">
         <OrgRecentErrors />
-        <Suspense key={query + sort} fallback={<OrgProjectsSkeleton />}>
-          <OrgProjects query={query} sort={sort} />
-        </Suspense>
+        <OrgProjects query={query} sort={sort} />
       </section>
     </>
   );
