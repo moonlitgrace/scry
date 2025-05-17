7499137a4aef7ceff80c055fbadecd60b1410cbd
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-17 09:46:34 +0530

    chore: dynamic tabs

--- apps/dashboard/app/org/[id]/settings/members/page.tsx
+++ apps/dashboard/app/org/[id]/settings/members/page.tsx
@@ -13,29 +13,38 @@ export default function Page() {
       <div className="flex flex-col gap-1">
         <h5 className="text-2xl font-bold">Members</h5>
         <span className="text-muted-foreground text-sm">
-          Manage team members and invitations
+          Manage organization members and invitations
         </span>
       </div>
       <InviteMembers />
-      <Tabs defaultValue="org-members">
+      <Tabs defaultValue="members">
         <TabsList className="gap-5 border-0 bg-transparent! p-0">
-          {['Org Members', 'Pending Invitations'].map((item) => {
-            const value = item.replaceAll(' ', '-').toLowerCase();
-            return (
-              <TabsTrigger
-                key={value}
-                value={value}
-                className="data-[state=active]:border-primary! rounded-none border-0! border-b-2! border-transparent! bg-transparent! p-0"
-              >
-                {item}
-              </TabsTrigger>
-            );
-          })}
+          {Object.entries(tabs).map(([value, { label, disabled }]) => (
+            <TabsTrigger
+              key={value}
+              value={value}
+              disabled={disabled}
+              className="data-[state=active]:border-primary! rounded-none border-0! border-b-2! border-transparent! bg-transparent! p-0"
+            >
+              {label}
+            </TabsTrigger>
+          ))}
         </TabsList>
-        <TabsContent value="org-members">
+        <TabsContent value="members">
           <OrgMembers />
         </TabsContent>
       </Tabs>
     </>
   );
 }
+
+const tabs = {
+  members: {
+    label: 'Members',
+    disabled: false,
+  },
+  'pending-invitations': {
+    label: 'Pending Invitations',
+    disabled: true,
+  },
+};
