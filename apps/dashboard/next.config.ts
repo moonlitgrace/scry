0dc8ec12dfb78a534e77441fc199e6c3063756d5
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-22 08:17:04 +0530

    chore: add

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
   turbopack: {
-    resolveAlias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@/public': path.resolve(__dirname, 'public'),
-    },
     rules: {
       '*.svg': {
         loaders: ['@svgr/webpack'],
