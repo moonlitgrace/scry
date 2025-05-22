5fb6f2d39ebf09076f1acd258ebc959ae0088f97
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-22 08:11:55 +0530

    fix: turbopack build alias resolve

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
   turbopack: {
+    resolveAlias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/public': path.resolve(__dirname, 'public'),
+    },
     rules: {
       '*.svg': {
         loaders: ['@svgr/webpack'],
